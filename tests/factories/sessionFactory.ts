2a72213efd1f24ca8784367bdd00c04b774bb715
ronald <ronaldjunior2@hotmail.com>
2021-08-13 18:10:30 -0300

    Preparando revisão de código

--- tests/factories/sessionFactory.ts
+++
@@ -1,8 +0,0 @@
-import { getRepository } from "typeorm";
-
-import Session from "../../src/entities/Session";
-
-export async function createSession(userId: number, token: string) {
-    const sessionRepository = getRepository(Session);
-    await sessionRepository.insert({ userId, token });
-}
